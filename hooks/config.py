084ed7bb9ca735a7f3a17903e8b7b68ccae9fcf2
cswimr <seaswimmerthefsh@gmail.com>
2025-05-29 16:50:12 -0500

    move config back into the mkdocs.yml file so other plugins read it properly

--- hooks/config.py
+++ hooks/config.py
@@ -1,15 +1,9 @@
 from datetime import datetime
-from os import getenv
 
 from mkdocs.config.defaults import MkDocsConfig
 
 
 def on_config(config: MkDocsConfig) -> MkDocsConfig:
-    config.site_url = getenv("MKDOCS_SITE_URL", config.site_url)
-    config.repo_name = getenv("MKDOCS_REPO_NAME", config.repo_name)
-    config.repo_url = getenv("MKDOCS_REPO_URL", config.repo_url)
-    config.edit_uri = getenv("MKDOCS_EDIT_URI", config.edit_uri)
-
     if not config.copyright:
         raise ValueError("The `copyright` option is not specified in mkdocs.yml!")
 
